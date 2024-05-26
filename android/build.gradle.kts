4b6c687f160178c3922c2377121a1cc5c1592c62
Thanos Psaridis <psaridis@gmail.com>
2024-05-26 17:47:31 +0300

    Updated to jdk 21 and kotlin 2.0.0 BlAzInGlY FAST!!!

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 //    implementation("androidx.activity:activity-compose:1.8.0")
     //implementation(platform("androidx.compose:compose-bom:2023.03.00"))
